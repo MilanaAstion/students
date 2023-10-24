aafb7100c5ca44fe28000943d9e99f9f01301881
MilanaAstion <milanaastion2004@gmail.com>
2023-10-24 16:03:12 +0300

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,11 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader'
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ]
   },
   resolve: {
